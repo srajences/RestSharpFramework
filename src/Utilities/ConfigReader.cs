24993ec73aca0bafb1037e2503963e94b260ddd8
Srajen Singh <srajen.singh@cesltd.com>
2024-04-15 09:19:38 +0530

    Added extent reports and inline parametrization support

--- src/Utilities/ConfigReader.cs
+++ src/Utilities/ConfigReader.cs
@@ -5,6 +5,8 @@ namespace APITESTFRAMEWORK
     public class ConfigReader
     {
         private const string ConfigPath = "C:\\Users\\srajen.singh\\Documents\\ApiFramework\\ApiTestFramework\\RestSharpFramework\\src\\Helpers\\Config.json";
+         private const string jsonResponsePath = "C:\\Users\\srajen.singh\\Documents\\ApiFramework\\ApiTestFramework\\RestSharpFramework\\src\\Helpers\\PostResponse.json";
+
         public string ReadUrl(string url){
             var json = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(ConfigPath));
             return json.GetProperty("ApiConstructor").GetProperty(url).GetString();
@@ -12,6 +14,10 @@ namespace APITESTFRAMEWORK
         public string ReadHeaders(string Headers){
             var json = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(ConfigPath));
             return json.GetProperty("ApiConstructor").GetProperty(Headers).GetString();
+        }
+            public string ReadId(string Id){
+            var json = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(jsonResponsePath));
+            return json.GetProperty(Id).GetString();
         }
     }
 }
--- test/E2ETest.cs
+++ test/E2ETest.cs
@@ -1,15 +1,21 @@
 using Xunit;
 using RestSharp;
 using System.Text.Json;
+using AventStack.ExtentReports;
 [assembly:TestCaseOrderer("APITESTFRAMEWORK.NumericTestCaseOrderer","APITESTFRAMEWORK")]
 namespace APITESTFRAMEWORK{
-public  class ApiTests{
+public  class E2ETest{
+    private ExtentReporter extentReporter;
+    public E2ETest(){
+        extentReporter = new ExtentReporter("index.html");
+    }
     private const string PostResponsePath = "C:\\Users\\srajen.singh\\Documents\\ApiFramework\\ApiTestFramework\\RestSharpFramework\\src\\Helpers\\PostResponse.json";
     private const string PostResponseSchemaPath = "C:\\Users\\srajen.singh\\Documents\\ApiFramework\\ApiTestFramework\\RestSharpFramework\\src\\Helpers\\PostRequestSchema.json";
 
         [Fact]
-        public void TestCase1()
+        public void TestCase1_CreatingUser()
         {
+            
             ConfigReader configReader = new ConfigReader();
             string endpointUrl = configReader.ReadUrl("Creating_User_Endpoint");
             string appId = configReader.ReadHeaders("app-id");
@@ -25,6 +31,8 @@ public  class ApiTests{
             };
             var jsonPayload = JsonSerializer.Serialize(payload);
             var client = new RestClient(endpointUrl);
+            extentReporter.createTest(nameof(TestCase1_CreatingUser));
+            extentReporter.LogStep(Status.Info,"Starting Test Case Execution");
             var request = new RestRequest("",Method.Post);
             request.AddHeader("app-id",appId);
             request.AddJsonBody(jsonPayload);
@@ -33,8 +41,32 @@ public  class ApiTests{
             var schemaJson = File.ReadAllText(PostResponseSchemaPath);
             var validateSchema = SchemaValidator.validateSchema(response.Content,schemaJson);
             Assert.True(validateSchema);
+            extentReporter.LogStep(Status.Pass, "Schema is validated");
             Assert.Equal(200,(int)response.StatusCode);
+            extentReporter.LogStep(Status.Pass, "Request is successfull");
+            extentReporter.FlushReport();
+
  }
+       
+        [Theory]
+        [InlineData(200)]
+        [InlineData(500)]
+        public void TestCase2_VerifyingUser(int statusCode){
+            ConfigReader configReader = new ConfigReader();
+            string endpointUrl = configReader.ReadUrl("Getting_User_Endpoint");
+            string appId = configReader.ReadHeaders("app-id");
+            string id = configReader.ReadId("id");
+            var client = new RestClient(endpointUrl+"/"+id);
+            extentReporter.createTest(nameof(TestCase2_VerifyingUser));
+            extentReporter.LogStep(Status.Info,"Starting Test Case Execution");
+            var request = new RestRequest("",Method.Get);
+            request.AddHeader("app-id",appId);
+            var response = client.Execute(request);
+            Assert.Equal(statusCode,(int)response.StatusCode);
+            extentReporter.LogStep(Status.Pass, "Request is successfull");
+            extentReporter.FlushReport();
+            
+        }
     }
 }
 
